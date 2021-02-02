97d86b767a173aa2ca24232226b410920743be4b
b20anne <b20anne@yahoo.fr>
2021-02-02 13:10:36 +0100

    test GET passed

--- app.js
+++ app.js
@@ -6,4 +6,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+
+
+app.get('/', (_, res) => res.json({ message: 'Hello World!' }));
+
 module.exports = app;
